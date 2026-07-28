9b9b2ee54888bcee13fed8c919ba872598ac775f
Vidyut Sriram <vidyut0712@gmail.com>
2026-07-28 11:08:14 +0530

    Even out FloatingLines wave brightness; smaller gutter arrows; brighter builds intro text

--- src/components/FloatingLines.jsx
+++ src/components/FloatingLines.jsx
@@ -90,7 +90,7 @@ vec3 getLineColor(float t) {
     gradientColor = mix(c1, c2, f);
   }
 
-  return gradientColor * 0.9;
+  return gradientColor * 0.7;
 }
 
 float wave(vec2 uv, float offset, vec2 screenUv, vec2 mouseUv, bool shouldBend) {
@@ -142,7 +142,7 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord) {
         baseUv,
         mouseUv,
         interactive
-      ) * 0.2;
+      ) * 0.4;
     }
   }
 
@@ -160,7 +160,7 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord) {
         baseUv,
         mouseUv,
         interactive
-      );
+      ) * 0.45;
     }
   }
 
@@ -179,7 +179,7 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord) {
         baseUv,
         mouseUv,
         interactive
-      ) * 0.1;
+      ) * 0.35;
     }
   }
 
